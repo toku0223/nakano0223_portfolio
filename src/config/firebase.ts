a8822dc3812cf6567511f40a78c67f2febf36c57
toku0223 <kei.nakanotoku@gmail.com>
2023-08-20 10:21:37 +0900

    main

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,22 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
 
 export const db = getFirestore();
